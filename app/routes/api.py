b1ebcb3e33a41a525878f5a8059290bb3beea301
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-02-27 18:49:58 -0500

    create user route sends failure to front

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 from app.models import User
 from app.db import get_db
+import sys
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -10,15 +11,22 @@ def signup():
     data = request.get_json()
     db = get_db()
 
-    # create a new user
-    newUser = User(
-        username=data['username'],
-        email=data['email'],
-        password=data['password']
-    )
+    try:
+        # attempt creating a new user
+        newUser = User(
+            username=data['username'],
+            email=data['email'],
+            password=data['password']
+        )
 
-    # save in database
-    db.add(newUser)
-    db.commit()
+        db.add(newUser)
+        db.commit()
+        print('success!')
+    except:
+        # insert failed, so send error to front end
+        print(sys.exc_info()[0])
+        # insert failed, so rollback and send error to front end
+        db.rollback()
+        return jsonify(message = 'Signup failed'), 500
 
-    return jsonify(id = newUser.id)
+    return jsonify(id=newUser.id)
