ce15dd111247f250e8394f1c99d8e7a5544e8d3e
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-02-27 16:41:30 -0500

    formatted all files

--- app/routes/home.py
+++ app/routes/home.py
@@ -2,14 +2,17 @@ from flask import Blueprint, render_template
 
 bp = Blueprint('home', __name__, url_prefix='/')
 
+
 @bp.route('/')
 def index():
-  return render_template('homepage.html')
+    return render_template('homepage.html')
+
 
 @bp.route('/login')
 def login():
-  return render_template('login.html')
+    return render_template('login.html')
+
 
 @bp.route('/post/<id>')
 def single(id):
-  return render_template('single-post.html')
+    return render_template('single-post.html')
