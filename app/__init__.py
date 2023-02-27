789baedb1656771341b05648a2a0d9a6767f90fb
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-02-27 16:54:48 -0500

    setup db session on site

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from app.routes import home, dashboard
+from app.db import init_db
 
 
 def create_app(test_config=None):
@@ -17,5 +18,7 @@ def create_app(test_config=None):
     # register routes
     app.register_blueprint(home)
     app.register_blueprint(dashboard)
+
+    init_db()
 
     return app
--- app/db/__init__.py
+++ app/db/__init__.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+from flask import g
 
 load_dotenv()
 
@@ -15,3 +16,11 @@ Base = declarative_base()
 
 def init_db():
     Base.metadata.create_all(engine)
+
+
+def get_db():
+    if 'db' not in g:
+        # store db connection in app context
+        g.db = Session()
+
+    return g.db
